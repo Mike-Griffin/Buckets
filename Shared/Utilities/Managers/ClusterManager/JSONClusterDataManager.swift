aa4fbffb75dde44dcfada0df4cec7eea0b3deed8
mike-griffin <mike_griffin@comcast.net>
2022-03-25 10:13:56 -0700

    Create and edit idea functional but not tested thoroughly

--- Shared/Utilities/Managers/ClusterManager/JSONClusterDataManager.swift
+++ Shared/Utilities/Managers/ClusterManager/JSONClusterDataManager.swift
@@ -24,9 +24,39 @@ struct JSONClusterDataManager: ClusterDataManager {
     
     func createIdea(name: String, bucket: Bucket?) {
         print("do it")
+        let idea = Idea(name: name)
+        if var bucket = bucket {
+            bucket.ideas.append(idea)
+            if let index = cluster.buckets.firstIndex(where: { $0.id == bucket.id }) {
+                cluster.buckets[index] = bucket
+            }
+        }
+        networkManager.saveCluster(cluster: cluster)
     }
     
     func editIdea(_ idea: Idea, name: String, bucket: Bucket?) {
         print("do it")
+        let newIdea = Idea(name: name)
+        for (i, b) in cluster.buckets.enumerated() {
+            if b.ideas.contains(idea) {
+                if bucket == nil || bucket!.id != b.id {
+                    // need to remove the idea from this bucket and add it to another
+                    let index = b.ideas.firstIndex(where: { $0.id == idea.id })!
+                    cluster.buckets[i].ideas.remove(at: index)
+                    if bucket != nil {
+                        if let bucketIndex = cluster.buckets.firstIndex(where: { $0.id == bucket!.id }) {
+                            cluster.buckets[bucketIndex].ideas.append(newIdea)
+                        }
+                            
+                    }
+                }
+                else {
+                    // just replace the idea in the bucket
+                    let index = b.ideas.firstIndex(where: { $0.id == idea.id })!
+                    cluster.buckets[i].ideas[index] = newIdea
+                }
+            }
+        }
+        networkManager.saveCluster(cluster: cluster)
     }
 }
